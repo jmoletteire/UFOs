6377d08a25373cdeb2b5a7d50e31fd0858c240e5
jmoletteire <jmoletteire@ufl.edu>
2022-08-25 13:59:26 -0400

    added filtered table functionality to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,9 @@
+/***** Const variable for the data in data.js *****/
 const tableData = data;
 
 /***** Reference the HTML table using D3 *****/
 var tbody = d3.select("tbody");
+
 
 function buildTable(data) {
     /***** Clear table *****/
@@ -58,3 +60,51 @@ function buildTable(data) {
 
     });
 }
+
+/***** Function to handle user input *****/
+function handleClick() {
+    /*************************************** 
+     * With 'd3.select("#datetime")', D3 will 
+     * look for the #datetime id in the HTML
+     * tags.
+     * 
+     * '.property("value")' tells D3 to not
+     * only look for where our date values
+     * are stored, but to also return that info
+     * and store it in 'date'.
+    ***************************************/
+    let date = d3.select("#datetime").property("value");
+
+
+    /*************************************** 
+     * Base Case
+     *  - If no date has been entered as a
+     *    filter, return all data/results.
+    ***************************************/
+    let filteredData = tableData;
+
+    /***************************************
+     * If date filter:
+     *  - Filter the data, showing only the 
+     *    rows where the date is equal to
+     *    the 'date' filter we created above.
+     **************************************/
+    if(date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+
+    /***** Build table with the filtered data *****/
+    buildTable(filteredData);
+}
+
+
+/************************************************** 
+ * Select all HTML tags with id=filter-btn, 
+ * when any of them are clicked, call handleClick().
+ *************************************************/
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+
+/***** Build original table when page first loads *****/
+buildTable(tableData);
